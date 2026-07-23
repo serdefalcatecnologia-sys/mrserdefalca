58dd6b9a8b25c9d027743461b8073c64e6bb9f01
serdefalcatecnologia-sys <serdefalcatecnologia@gmail.com>
2026-07-23 08:36:31 -0400

    zxczx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,9 +28,9 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        {children}
-      </body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+  {children}
+</body>
     </html>
   );
 }
