7363dcc36d7678a014dd5fea189f2c76dc7b4a71
Joswar Valles <serdefalcatecnologia@gmail.com>
2026-09-22 10:10:06 -0500

    feat: actualizar la implementación de Supabase para usar createBrowserClient

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
